3ccbf48b50232639b468c94c3ae3aab5136eccc3
Mthokozisi Masondo <m.masondo2001@gmail.com>
2024-10-24 21:23:36 +0200

    debugged and added error handling

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
-
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const velocityPerHour = 10000; // initial velovity (km/h)
+const acceleration = 3; // m/s^2
+const timeInSeconds = 3600; // 1 hour
+const distanceInKilometres = 0;
+const initialFuelInKilograms = 5000; 
+const fuelBurnRate = 0.5; // kg/s
 
+// Calculating new values
+const newDistance = distanceInKilometres + (velocityPerHour * (timeInSeconds / 3600)) 
+const remainingFuel = initialFuelInKilograms - (fuelBurnRate * timeInSeconds)
+const newVelocity = calculateNewVelocity(velocityPerHour, acceleration, timeInSeconds) 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+function calculateNewVelocity (velocity, acceleration, time) { 
+try {
+  // Ensure that three values are passed to the function when it's called
+  if (arguments.length !== 3) {
+    throw new Error("Three arguments are needed when calling the function.")
+  }
 
-// Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+  // Ensure that every argument is a number
+  if (typeof velocity !== 'number' || typeof acceleration !== 'number' || typeof time !== 'number') {
+    throw new Error("All three arguments need to be numbers.")
+  }
+
+  // Calculation of the new velocity
+  return velocity + (acceleration * time) 
+
+} catch (error) {
+  // Errors when the functions is executed
+  console.error(error.message)
+  return null 
+  }
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+// Logging the results to the console
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
